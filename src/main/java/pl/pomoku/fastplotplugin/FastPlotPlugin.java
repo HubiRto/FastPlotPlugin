ed5499cecef639b1b06038bb709f29fea8ccd8e7
hubert <hubert.rybicki.hr@gmail.com>
2023-06-06 09:31:25 +0200

    Initial commit

---
+++ src/main/java/pl/pomoku/fastplotplugin/FastPlotPlugin.java
@@ -0,0 +1,17 @@
+package pl.pomoku.fastplotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FastPlotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
